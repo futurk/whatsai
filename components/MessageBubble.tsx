b76c7c7c051d9f766ba0a5b679388529212ed62f
Furkan Ünlütürk <unluturkfurkan@proton.me>
2025-05-28 16:23:42 +0300

    Updated MessageBubble.tsx

--- components/MessageBubble.tsx
+++ components/MessageBubble.tsx
@@ -53,7 +53,7 @@ const MessageBubble = ({ message, agentColor, agentName, conversationId, isLastU
       setShowCopyButtonCallback = setShowCopyButton;
       copyButtonOpacity.value = withSpring(1, { damping: 15 });
     } else {
-      copyButtonOpacity.value = withTiming(0, { duration: 100 });
+      copyButtonOpacity.value = withTiming(0, { duration: 500 });
     }
 
     return () => {
