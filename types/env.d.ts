14a8ea100fb7070e1671c90d195a06c1536efbb0
Furkan Ünlütürk <unluturkfurkan@proton.me>
2025-06-29 23:35:46 +0300

    Updated env.d.ts

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,11 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      EXPO_PUBLIC_API_URL: string;
-      EXPO_PUBLIC_OPENAI_API_KEY: string;
-      EXPO_PUBLIC_ANTHROPIC_API_KEY: string;
       EXPO_PUBLIC_SAMPLE_OPENAI_KEY: string;
-      EXPO_PUBLIC_SAMPLE_FALSE_KEY: string;
+      EXPO_PUBLIC_SAMPLE_ANTHROPIC_KEY: string;
     }
   }
 }
